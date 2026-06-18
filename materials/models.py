70940c37bdb38b3e151a467d2f6683eacc288cb5
Лусине <lumahinyan@mail.ru>
2026-06-18 22:55:23 +0300

    Добавлены модели owner и базовая структура permissions

--- materials/models.py
+++ materials/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from users.models import User
 
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
     preview = models.ImageField(upload_to='course_previews/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
@@ -22,5 +30,13 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True)
     video_url = models.URLField()
 
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
     def __str__(self):
         return self.name
+
--- materials/views.py
+++ materials/views.py
@@ -7,7 +7,13 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
