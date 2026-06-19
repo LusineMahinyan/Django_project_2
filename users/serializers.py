46559c823a97994987d963ca89a348f478fe3017
Лусине <lumahinyan@mail.ru>
2026-06-19 22:52:31 +0300

    Homework 30.2 final: payments, nested serializers, filters, fixtures

--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,42 @@
 from rest_framework import serializers
 from .models import User, Payment
+from materials.models import Course, Lesson
 
 
-class UserSerializer(serializers.ModelSerializer):
+class CoursePaymentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = "__all__"
+        model = Course
+        fields = ("id", "name")
+
+
+class LessonPaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ("id", "name")
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    paid_course = CoursePaymentSerializer(read_only=True)
+    paid_lesson = LessonPaymentSerializer(read_only=True)
+
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "email",
+            "phone",
+            "city",
+            "avatar",
+            "payments",
+        )
