0963e854a3d091f77442aa2334b7e14d8c031779
Лусине <lumahinyan@mail.ru>
2026-06-20 16:28:56 +0300

    Домашняя работа 33

--- materials/models.py
+++ materials/models.py
@@ -3,8 +3,17 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
-    preview = models.ImageField(upload_to='course_previews/', blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    preview = models.ImageField(
+        upload_to="course_previews/",
+        blank=True,
+        null=True
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -19,8 +28,31 @@ class Lesson(models.Model):
 
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True)
+    preview = models.ImageField(
+        upload_to='lesson_previews/',
+        blank=True,
+        null=True
+    )
     video_url = models.URLField()
 
     def __str__(self):
         return self.name
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="subscriptions"
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="subscriptions"
+    )
+
+    class Meta:
+        unique_together = ("user", "course")
+
+    def __str__(self):
+        return f"{self.user} -> {self.course}"
--- materials/views.py
+++ materials/views.py
@@ -1,12 +1,17 @@
 from rest_framework import viewsets, generics
+
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
-
+from .tasks import send_course_update_email
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def perform_update(self, serializer):
+        course = serializer.save()
+        send_course_update_email.delay(course.id)
 
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
@@ -14,7 +19,7 @@ class LessonListCreateAPIView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         queryset = Lesson.objects.all()
-        course_id = self.request.query_params.get('course')
+        course_id = self.request.query_params.get("course")
 
         if course_id:
             queryset = queryset.filter(course_id=course_id)
@@ -22,6 +27,8 @@ class LessonListCreateAPIView(generics.ListCreateAPIView):
         return queryset
 
 
-class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+class LessonRetrieveUpdateDestroyAPIView(
+    generics.RetrieveUpdateDestroyAPIView
+):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
